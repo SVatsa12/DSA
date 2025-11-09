65362af193ed4db7cbeb18f31cab58d15359458f
SVatsa12 <qshubhamq2@gmail.com>
2025-11-10 01:40:11 +0530

    continueStatements

---
+++ .dist/ContinueStatements.java
@@ -0,0 +1,10 @@
+public class ContinueStatements {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            if(i==2){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
