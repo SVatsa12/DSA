d3b7db4e84b42917893150ab55b8f0da2e6b723b
SVatsa12 <qshubhamq2@gmail.com>
2026-03-07 20:33:58 +0530

    initial commit

---
+++ ReverseNegNumber.java
@@ -0,0 +1,19 @@
+public class ReverseNegNumber {
+    public static int ReverseNegative(int n){
+        int digit=0;
+        int rev=0;
+        while(n!=0){
+            digit=n%10;
+            if(rev>Integer.MAX_VALUE/10 || rev<Integer.MIN_VALUE/10){ //handles integer overflow
+                return 0;
+            }
+            rev=rev*10+digit;
+            n=n/10;
+        }
+        return rev;
+    }
+    public static void main(String[] args) {
+        int num=-123;
+        System.out.println(ReverseNegative(num));
+    }
+}
