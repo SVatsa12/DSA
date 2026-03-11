ed586314ad6c70a9ad30c2f0faa671cb08d3d118
SVatsa12 <qshubhamq2@gmail.com>
2026-03-12 02:08:05 +0530

    done

--- PrintSubArrays.java
+++ PrintSubArrays.java
@@ -7,15 +7,29 @@ public class PrintSubArrays {
                 int end=j;
                 for(int k=start;k<=end;k++){
                     System.out.print(arr[k]+" ");
-                    cnt++;
                 }
+                cnt++;
                 System.out.println();
             }
         }
         System.out.println("Total subarrays:"+cnt);
     }
+public static void PrintSubarrays2(int arr[]) {
+    int cnt=0;
+    for (int i = 0; i < arr.length; i++) {
+        String subarray = "";
+        for (int j = i; j < arr.length; j++) {
+            subarray += arr[j] + " ";
+             cnt++;
+            System.out.println(subarray.trim());
+           
+        }
+    }
+    System.out.println("Total subarrays:"+cnt);
+}
     public static void main(String[] args) {
         int arr[]={2,4,6,8,10};
         PrintSubarrays(arr);
+        PrintSubarrays2(arr);
     }
 }
