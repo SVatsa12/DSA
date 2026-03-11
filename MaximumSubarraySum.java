191eea4b7a0734eea1303d4e1d010b0352e59d7e
SVatsa12 <qshubhamq2@gmail.com>
2026-03-12 02:41:57 +0530

    done

--- MaximumSubarraySum.java
+++ MaximumSubarraySum.java
@@ -16,8 +16,22 @@ public class MaximumSubarraySum {
         
         System.out.println("Max sum:"+maxSum);
     }
+    public static int MaximumSubarraySum2(int arr[]){
+        int currSum=arr[0];
+        int maxSum=arr[0];
+        if(arr.length==1){
+            return arr[0];
+        }
+        for(int i=0;i<arr.length;i++){
+            currSum=Math.max(arr[i],currSum+arr[i]);
+            maxSum=Math.max(maxSum, currSum);
+        }
+        return maxSum;
+    }
     public static void main(String[] args) {
         int arr[]={-2,1,-3,4,-1,2,1,-5,4};
         MaxSubarraySum(arr);
+        int x=MaximumSubarraySum2(arr);
+        System.out.println(x);
     }
 }
