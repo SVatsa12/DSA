3f5640ad1452e6690f0ebcb13df4a474743bb874
SVatsa12 <qshubhamq2@gmail.com>
2026-03-18 02:17:29 +0530

    done

--- PalindromeString.java
+++ PalindromeString.java
@@ -12,18 +12,31 @@ public class PalindromeString {
         }
         return true;
     }
-    // public static boolean isPalindromeLEETCODE(String s){
-    //     int n=s.length();
-    //     int i=0;
-    //     int j=n-1;
-    //     while(i<j){
-
-    //     }
-    // }
+    public static boolean isPalindromeLEETCODE(String s){
+        int n=s.length();
+        int i=0;
+        int j=n-1;
+        while(i<j){
+            while(i<j && !Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+            }
+            while(i<j && !Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            }
+            if(Character.toLowerCase(s.charAt(i))!=Character.toLowerCase(s.charAt(j))){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
     public static void main(String[] args) {
         String str1="hello";
         String str2="madam";
         System.out.println(isPalindrome(str1));
         System.out.println(isPalindrome(str2));
+        System.out.println(isPalindromeLEETCODE(str1));
+        System.out.println(isPalindromeLEETCODE(str2));
     }
 }
