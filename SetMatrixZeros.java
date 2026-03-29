57d910eab2b49f87a228079b2c17864a6efa5270
SVatsa12 <qshubhamq2@gmail.com>
2026-03-29 22:22:55 +0530

    solved

--- SetMatrixZeros.java
+++ SetMatrixZeros.java
@@ -49,6 +49,56 @@ public class SetMatrixZeros {
             }
         }
     }
+    //optimal
+    public static void setMatrixZero3(int [][]matrix){
+        int m=matrix.length;
+        int n=matrix[0].length;
+        boolean firstRowZero=false;
+        boolean firstColZero=false;
+        //check first row
+        for(int j=0;j<n;j++){
+            if(matrix[0][j]==0){
+                firstRowZero=true;
+                break;
+            }
+        }
+        //check first col
+        for(int i=0;i<m;i++){
+            if(matrix[i][0]==0){
+                firstColZero=true;
+                break;
+            }
+        }
+        //use first row and columns as markers
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;   //mark row
+                    matrix[0][j]=0;  //mark column
+                }
+            }
+        }
+        //set zeros  based on marker
+        for(int i=0;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        //zero the first col if needed
+        if(firstColZero){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
+            }
+        }
+        if(firstRowZero){
+            for(int j=0;j<n;j++){
+                matrix[0][j]=0;
+            }
+        }
+
+    }
     public static void main(String[] args) {
        int matrix[][] = {
         {1, 1, 1},
@@ -56,6 +106,11 @@ public class SetMatrixZeros {
         {1, 1, 1}
     };
     int matrix2[][] = {
+        {1, 1, 1},
+        {1, 0, 1},
+        {1, 1, 1}
+    };
+    int matrix3[][] = {
         {1, 1, 1},
         {1, 0, 1},
         {1, 1, 1}
@@ -70,6 +125,13 @@ public class SetMatrixZeros {
         System.out.println();
     }
         SetMatrixZero2(matrix2);
+    for(int i = 0; i < matrix.length; i++) {
+        for(int j = 0; j < matrix[0].length; j++) {
+            System.out.print(matrix[i][j] + " ");
+        }
+        System.out.println();
+    }
+            setMatrixZero3(matrix3);
     for(int i = 0; i < matrix.length; i++) {
         for(int j = 0; j < matrix[0].length; j++) {
             System.out.print(matrix[i][j] + " ");
