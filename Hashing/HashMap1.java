3f86e48f7cebfc4496183e58773ee4cd8d8cac54
SVatsa12 <qshubhamq2@gmail.com>
2026-04-05 20:36:22 +0530

    solved

--- Hashing/HashMap1.java
+++ Hashing/HashMap1.java
@@ -3,10 +3,18 @@ import java.util.*;
 public class HashMap1 {
     public static void main(String[] args) {
         HashMap<String,Integer>hm=new HashMap<>();
+        //put method
         hm.put("Delhi", 1);
         hm.put("Mumbai", 2);
         hm.put("Hyderabad", 3);
         hm.put("Chennai", 4);
         System.out.println(hm);
+
+        //get method
+        System.out.println(hm.get("Chennai"));
+
+        //size method
+        System.out.println(hm.size());
+
     }
 }
