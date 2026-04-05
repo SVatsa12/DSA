5667c77111ea2df734b359a86afbdedfaae0965f
SVatsa12 <qshubhamq2@gmail.com>
2026-04-05 20:34:45 +0530

    hashing

---
+++ Hashing/HashMap1.java
@@ -0,0 +1,12 @@
+package Hashing;
+import java.util.*;
+public class HashMap1 {
+    public static void main(String[] args) {
+        HashMap<String,Integer>hm=new HashMap<>();
+        hm.put("Delhi", 1);
+        hm.put("Mumbai", 2);
+        hm.put("Hyderabad", 3);
+        hm.put("Chennai", 4);
+        System.out.println(hm);
+    }
+}
