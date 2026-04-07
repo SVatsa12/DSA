4a288da824b3bf77f34ffc706aacbb8c14419b15
SVatsa12 <qshubhamq2@gmail.com>
2026-04-07 21:25:46 +0530

    solved

---
+++ Hashing/LinkedHashMapImplementation.java
@@ -0,0 +1,14 @@
+package Hashing;
+import java.util.*;
+public class LinkedHashMapImplementation {
+    public static void main(String[] args) {
+        LinkedHashMap<Integer,Integer>lhm=new LinkedHashMap<>();
+        lhm.put(1,50);
+        lhm.put(2,100);
+        lhm.put(3,150);
+        lhm.put(4,200);
+        lhm.put(5,250);
+        System.out.println(lhm);
+
+    }
+}
