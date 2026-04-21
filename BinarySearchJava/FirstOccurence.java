15a9e6ad2b0e8eaa446ead07a8d2fa8590b9c6dd
SVatsa12 <qshubhamq2@gmail.com>
2026-04-21 16:23:29 +0530

    firstoccurence

---
+++ BinarySearchJava/FirstOccurence.java
@@ -0,0 +1,27 @@
+package BinarySearchJava;
+
+public class FirstOccurence {
+    public static int Firstoccurence(int arr[],int target){
+        int low=0;
+        int high=arr.length-1;
+        int ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]==target){
+                ans=mid;
+                high=mid-1;
+            }else if(arr[mid]>target){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int arr[]={3, 4, 13, 13, 13, 20, 40};
+        int target=13;
+        int res=Firstoccurence(arr, target);
+        System.out.println(res);
+    }
+}
