b49556ba917a176c399383c17c962d6c5614a4b9
SVatsa12 <qshubhamq2@gmail.com>
2026-04-27 23:44:12 +0530

    LinkedHashMap iteration

---
+++ Hashing/LinkedHashMapIteration.java
@@ -0,0 +1,19 @@
+
+package Hashing;
+import java.util.*;
+public class LinkedHashMapIteration {
+    public static void main(String[] args) {
+        LinkedHashMap<String,Integer>lhm=new LinkedHashMap<>();
+        lhm.put("A",100);
+        lhm.put("B",150);
+        lhm.put("C",200);
+        lhm.put("D",250);
+
+        //Iteration using entrySet()
+        for(Map.Entry<String,Integer> entry:lhm.entrySet()){
+            System.out.println(entry.getKey()+"->"+entry.getValue());
+        }
+        
+
+    }
+}
