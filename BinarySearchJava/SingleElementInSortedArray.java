8da9d81897e9adfe9cc37730b0bde85cee1d0f90
SVatsa12 <qshubhamq2@gmail.com>
2026-04-27 23:32:23 +0530

    solved

--- BinarySearchJava/SingleElementInSortedArray.java
+++ BinarySearchJava/SingleElementInSortedArray.java
@@ -1,8 +1,25 @@
 package BinarySearchJava;
 
 public class SingleElementInSortedArray {
+    public static int singleNonduplicate(int arr[]){
+        int low=0;
+        int high=arr.length-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(mid%2==1)mid--;
+               if(arr[mid]==arr[mid+1]){
+                low=mid+2;
+            }else{
+                high=mid;
+            }
+        }
+        return arr[low];
+    }
+
     public static void main(String[] args) {
-        int arr[]={};
+        int arr[]={1,1,2,3,3,4,4,8,8};
+        int res=singleNonduplicate(arr);
+        System.out.println(res);
         
     }
 }
