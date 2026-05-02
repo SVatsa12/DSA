bdb815352c4a46b9091c6de979a28650768e8177
SVatsa12 <qshubhamq2@gmail.com>
2026-05-02 22:26:36 +0530

    solved

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PascalTriangle {
+    //optimised
     public static List<List<Integer>> generate(int numRows){
         List<List<Integer>>res=new ArrayList<>();
         for(int i=1;i<=numRows;i++){
