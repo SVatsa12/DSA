d2013f9460f5f9c79fa58adaef21892f50465615
SVatsa12 <qshubhamq2@gmail.com>
2026-05-07 02:06:47 +0530

    LinkedList

---
+++ LinkedList/LinkedList2.java
@@ -0,0 +1,63 @@
+package LinkedList;
+
+// add node in the specific index
+public class LinkedList2 {
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addAtIndex(int data, int idx) {
+        if (idx == 0) {
+            addFirst(data);
+            return;
+        }
+        Node newnode = new Node(data);
+        Node temp = head;
+        int i = 0;
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+        newnode.next = temp.next;
+        temp.next = newnode;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LinkedList2 ll = new LinkedList2();
+        ll.addFirst(10);
+        ll.addFirst(20);
+        ll.addFirst(30);
+        ll.addFirst(40);
+        ll.addFirst(50);
+        ll.addAtIndex(30, 0);
+        ll.printList();
+    }
+}
