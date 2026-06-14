ef5f707c11edaed13dc6324c2242cb5f7e2938ee
SVatsa12 <qshubhamq2@gmail.com>
2026-06-14 23:16:07 +0530

    cycle

---
+++ LinkedList/CycleDetection.java
@@ -0,0 +1,44 @@
+package LinkedList;
+
+public class CycleDetection {
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static boolean DetectCycle(Node head){
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        Node n1 = new Node(10);
+        Node n2 = new Node(20);
+        Node n3 = new Node(30);
+        Node n4 = new Node(40);
+        Node n5 = new Node(50);
+        Node n6 = new Node(60);
+        Node n7 = new Node(70);
+        head = n1;
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n6;
+        n6.next = n7;
+        // Create a cycle: 70 -> 30
+        n7.next = n3;
+        System.out.println("Cycle Present: " + DetectCycle(head));
+    }
+}
